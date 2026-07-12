6b1526f940499d63cdf527d723ffa3cbd99b9500
Atul Khandekar <125445267+Atul-Khandekar@users.noreply.github.com>
2026-07-12 22:51:20 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+    
+    for (i in digits.indices.reversed()) {
+        if (digits[i] < 9) {
+            digits[i] += 1
+            return digits
+        }
+        
+        digits[i] = 0
+    }
+    
+
+    val result = IntArray(digits.size + 1)
+    result[0] = 1
+    return result
+}
+}
