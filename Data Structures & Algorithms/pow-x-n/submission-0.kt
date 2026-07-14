02faffd414979f7a33149f1387cba92eaf94770b
Atul Khandekar <125445267+Atul-Khandekar@users.noreply.github.com>
2026-07-14 23:09:55 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun myPow(x: Double, n: Int): Double {
+        var base = x
+        var exp = n.toLong() 
+
+        if (exp < 0) {
+            base = 1.0 / base
+            exp = -exp
+        }
+
+        var result = 1.0
+        while (exp > 0) {
+           
+            if (exp % 2 == 1L) {
+                result *= base
+            }
+           
+            base *= base
+            exp /= 2
+        }
+
+        return result
+    }
+}
